23b599183103272f59f60374584c6c8f57b5cbab
balmaster <iam.balmaster@gmail.com>
2012-09-30 13:09:31 +0400

    set upload_to by standart scheme of feincms os.path.join(settings.FEINCMS_UPLOAD_PREFIX, 'articles/thumbnails')
    we need one place for uploading, because that this required correct user rights on fs

--- articles/extensions/thumbnail.py
+++ articles/extensions/thumbnail.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from feincms import settings
 
 def register(cls, admin_cls):
-    cls.add_to_class('thumbnail', models.ImageField(_('thumbnail'), max_length=250, upload_to="images/articles/thumbnails", null=True, blank=True))
+    cls.add_to_class('thumbnail', models.ImageField(_('thumbnail'), max_length=250, upload_to=os.path.join(settings.FEINCMS_UPLOAD_PREFIX, 'articles/thumbnails'), null=True, blank=True))
     if admin_cls:
         if admin_cls.fieldsets:
             admin_cls.fieldsets[0][1]['fields'].append('thumbnail')
